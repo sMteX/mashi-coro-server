e2ba885d83bc1f11427a522213b23d290b014dab
Lukáš Stuchlík <stuchlik1@seznam.cz>
2020-02-17 00:11:03 +0100

    Mapping to env variables, changed TypeORM config for prod?

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const host: string = config.get('server.host');
-    const port: number = config.get('server.port');
+    const port: number = config.get('server.port'); // should map process.env.PORT with highest precedence
 
     const app = await NestFactory.create(AppModule, { cors: true });
-    await app.listen(port, host);
+    if (process.env.NODE_ENV === 'production') {
+        await app.listen(port);
+    } else {
+        const host: string = config.get('server.host');
+        await app.listen(port, host);
+    }
 }
 
 bootstrap();
